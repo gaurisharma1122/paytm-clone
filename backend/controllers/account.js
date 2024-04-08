c8b5afad0e21e8bf81ab804f08cba8dc6cd6c9c1
Gauri Sharma <gaurisharma1122@gmail.com>
2024-04-09 01:49:23 +0530

    added transfer money endpoint and frontend folder structure

--- backend/controllers/account.js
+++ backend/controllers/account.js
@@ -1,4 +1,5 @@
-const { Account } = require("../db");
+const mongoose = require('mongoose');
+const { Account, User } = require("../db");
 
 const getBalance = async (req, res) => {
     try {
@@ -21,4 +22,44 @@ const getBalance = async (req, res) => {
     }
 }
 
-module.exports = { getBalance };
+const transferMoney = async (req, res) => {
+    try {
+        const { to, amount } = req.body;
+        const userId = req.userId;
+        const userAccount = await Account.findOne({ userId });
+        const receiverAccount = await Account.findOne({ userId: to });
+        if (!receiverAccount || !userAccount) {
+            return res.status(411).json({
+                message: `${ !receiverAccount ? 'Receiver' : 'User' } account not found`
+            });
+        }
+        const session = await mongoose.startSession();
+        await session.startTransaction();
+
+        if (amount > userAccount.balance) {
+            await session.abortTransaction();
+            return res.status(411).json({
+                message: 'Insufficient Balance'
+            });
+        }
+
+        await Account.updateOne({ userId }, { $inc: { balance: -amount } }).session(session);
+        await Account.updateOne({ userId: to }, { $inc: { balance: amaount } }).session(session);
+
+        await session.commitTransaction();
+
+        return res.status(200).json({
+            message: "Money transferred successfully",
+            
+        });
+        
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).json({
+            message: error.message
+        });
+    }
+}
+
+module.exports = { getBalance, transferMoney };
--- backend/router/account.js
+++ backend/router/account.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { getBalance } = require('../controllers/account');
+const { getBalance, transferMoney } = require('../controllers/account');
 const { userAuthMiddleware } = require('../middlewares/user');
 const router = Router();
 
 router.get('/getBalance', userAuthMiddleware, getBalance);
+router.post('/transferMoney',userAuthMiddleware, transferMoney);
 
 
 module.exports = router;
